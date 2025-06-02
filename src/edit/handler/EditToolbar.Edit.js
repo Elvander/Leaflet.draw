432a39699e0d1d2e6e4eddaa69f833c3b8b937e5
koenvanloock <koen@jdsoft.be>
2025-06-02 09:27:56 +0200

    removed log

--- src/edit/handler/EditToolbar.Edit.js
+++ src/edit/handler/EditToolbar.Edit.js
@@ -255,9 +255,6 @@ L.EditToolbar.Edit = L.Handler.extend({
 					delete layer.options.previousOptions;
 				}
 			}
-
-			console.log('hier geraken we')
-
 			if (layer instanceof L.Marker) {
 				if(layer.dragging) {
 					layer.dragging.disable();
